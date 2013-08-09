b4d946e366140f0a28b279e45d8161074e79f074
Felipe Talavera <felipe.talavera@gmail.com>
2013-08-09 12:02:31 +0200

    add alfred2 support

---
+++ sprout-osx-apps/attributes/alfred2.rb
@@ -0,0 +1,1 @@
+node.default["alfred2_download_uri"]="http://cachefly.alfredapp.com/Alfred_2.0.7_205.zip"
---
+++ sprout-osx-apps/recipes/alfred2.rb
@@ -0,0 +1,12 @@
+unless File.exists?("/Applications/Alfred\ 2.app")
+  remote_file "#{Chef::Config[:file_cache_path]}/Alfred_2.0.7_205.zip" do
+    source "http://cachefly.alfredapp.com/Alfred_2.0.7_205.zip"
+    mode "0644"
+  end
+
+  execute "unzip alfred2" do
+    command "unzip #{Chef::Config[:file_cache_path]}/Alfred_2.0.7_205.zip Alfred\ 2.app/* -d /Applications/"
+    user node['current_user']
+    group "admin"
+  end
+end
