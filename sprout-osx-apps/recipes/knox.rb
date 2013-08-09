84609538f7863f9c93f23053a8e3453b56e80f38
Felipe Talavera <felipe.talavera@gmail.com>
2013-08-09 12:17:59 +0200

    wrong zip for knox

--- sprout-osx-apps/recipes/knox.rb
+++ sprout-osx-apps/recipes/knox.rb
@@ -4,8 +4,8 @@ unless File.exists?("/Applications/Knox.app")
     mode "0644"
   end
 
-  execute "unzip alfred2" do
-    command "unzip #{Chef::Config[:file_cache_path]}/Alfred_2.0.7_205.zip Knox.app/* -d /Applications/"
+  execute "unzip knox" do
+    command "unzip #{Chef::Config[:file_cache_path]}/Knox-2.2.0.zip Knox.app/* -d /Applications/"
     user node['current_user']
     group "admin"
   end
